af5bf788ab99bb0102752e813aeac8df84a7bc21
Jéssica de Medeiros <77396504+jessica-jmc@users.noreply.github.com>
2023-06-23 01:23:01 -0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insira a temperatura em Celsius: ");
+        int temperatura = scanner.nextInt();
+        double resultado = (temperatura * 1.8) + 32;
+        String mensagem = String.format("O resultado da conversão de Celsius %d é %.2f Fahrenheit!", temperatura, resultado);
+        System.out.println(mensagem);
     }
 }
